38715b759fb8b8b9fca8fbcce197952c26b3add6
寺内 信夫 <tenobu@gmail.com>
2015-12-15 21:29:11 +0900

    First Init 2

---
+++ Explorer/Explorer/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Explorer
+//
+//  Created by 寺内 信夫 on 2015/12/15.
+//  Copyright © 2015年 寺内 信夫. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(aNotification: NSNotification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(aNotification: NSNotification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+

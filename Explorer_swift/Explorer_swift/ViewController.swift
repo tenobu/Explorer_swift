e394bec84566f4a980afee32803c1b2ab78784b1
寺内 信夫 <tenobu@gmail.com>
2015-12-15 20:33:24 +0900

    First init

---
+++ Explorer_swift/Explorer_swift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Explorer_swift
+//
+//  Created by 寺内 信夫 on 2015/12/15.
+//  Copyright © 2015年 寺内 信夫. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: AnyObject? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+

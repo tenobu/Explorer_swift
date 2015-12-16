43cf728318375a6a94f64752c1905b44be4c19b7
寺内 信夫 <tenobu@gmail.com>
2015-12-16 22:50:11 +0900

    DirectoryObj 初期化

---
+++ Explorer/Explorer/DirectoryObj.swift
@@ -0,0 +1,14 @@
+//
+//  DirectryObj.swift
+//  Explorer
+//
+//  Created by 寺内 信夫 on 2015/12/16.
+//  Copyright © 2015年 寺内 信夫. All rights reserved.
+//
+
+import Foundation
+
+class DirectryObj {
+	
+	
+}
